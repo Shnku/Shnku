a9215d2f939043ce1a003884893c60b72d78a594
ShAnku <shankubag04@gmail.com>
2022-12-01 09:30:24 +0000

    ok

--- evenoddseries.c
+++ evenoddseries.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 int evenodd(int);
-void disp_oddevn_array(int[],int[]);
+//void disp_oddevn_array(int[],int[]);
 
 int main()
 {
     int n,i;
     int evn=0,odd=0;
-    int evn_arr[100],odd_arr[100];
+    int evn_arr[100],odd_arr[100],size;
     printf(("en the range: "));
     scanf("%d",&n);
     for (i=1; i<=n; i++) {
         if (evenodd(i)==1) {
-            evn_arr[evn]=i;
-            evn++;
+            evn_arr[evn++]=i;
+            //evn++;
         }
         else{
-            odd_arr[odd]=i;
-            odd++;
+            odd_arr[odd++]=i;
+            //odd++;
         }
     }
     printf("there are %d odd & %d even ons\n",evn,odd);
     
-    int size= sizeof(evn_arr[100])+sizeof(odd_arr[100]);
-    for (int i=0; i<=size; i++) {
+    // size= sizeof(evn_arr[100])+sizeof(odd_arr[100]);
+    // printf("\nthe size is %d\n",size);
+    // size= sizeof(evn_arr[100]+odd_arr[100]);
+    // printf("the size is %d\n",size);
+    //disp_oddevn_array(evn_arr,odd_arr);
+    
+    for (int i=0,j=0; i<evn && j<odd; i++,j++) {
         printf("%d\t%d\n",evn_arr[i],odd_arr[i]);     
     }
-    
-    disp_oddevn_array(evn_arr,odd_arr);
+    printf("..............................\n");
     return 0;
 }
 
@@ -37,12 +41,13 @@ int evenodd(int no){
         return 0;    
 }
 
-void disp_oddevn_array(int a[100],int b[100])
-{
-    int size= sizeof(a[100])+sizeof(b[100]);
-    for (int i=0; i<size; i++)
-    { 
-        printf("%d\t%d\n",a[i],b[i]);
-    }
+//void disp_oddevn_array(int a[100],int b[100])
+//{
+//    // int size= sizeof(a[100])+sizeof(b[100]);//wrong because it returns size of int
+
+//    for (int i=0; i<size; i++)
+//    { 
+//        printf("%d\t%d\n",a[i],b[i]);
+//    }
     
-}
+//}
