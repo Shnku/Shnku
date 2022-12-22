9d4a22ba67438540045877fe79ba2850edd07152
ShAnku <shankubag04@gmail.com>
2022-12-22 14:25:06 +0000

    ok

---
+++ sum_of_digit.c
@@ -0,0 +1,16 @@
+
+
+#include <stdio.h>
+int main() {
+
+  int a, s = 0, i = 0, r;
+//  printf("en the value");
+  scanf("%d", &a);
+  while (a != 0) {
+    r = a % 10;
+    s = s + r;
+    a = a / 10;
+  }
+  printf("%d\n", s);
+  return 0;
+}
