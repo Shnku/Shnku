9a44cc6b4390ac4e6b350e0f1b1de86cd09f088d
ShAnku <shankubag04@gmail.com>
2023-01-25 09:20:00 +0000

    b

---
+++ windowcusror.c
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
+//#include <unistd.h>
+//#include <termios.h>
+
+// gotoxy() function definition
+void gotoxy(int x, int y) { printf("%c[%d;%df", 0x1B, y, x); }
+
+//void clrscr(void) { system("clear"); }
+
+int main() {
+  system("clear");
+  int x = 10, y = 20;
+  gotoxy(3, y);                 // move cursor position
+  printf("Hello World!!!\n\n"); // print message
+  return 0;
+}
