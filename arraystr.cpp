dd35cc5b95d0fe3867b2982d147f5f9cdbc5f55b
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-02-25 12:01:17 +0000

    .

--- arraystr.cpp
+++ arraystr.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  const int n=0;
+int main()
+{
+  const int n = 0;
   int arr[n];
   cout << "en the value of n: ";
   cin >> n;
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     cout << "en the" << i << "th term to array: ";
     cin >> arr[i];
   }
