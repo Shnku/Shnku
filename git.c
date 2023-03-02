123899f703a47ab0c1c0487c965b19676cd08d49
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-03-02 14:48:44 +0000

    .

--- git.c
+++ git.c
@@ -7,7 +7,7 @@ int main()
     printf(".............................\n");
 
     system("git add --all");
-    system("git commit -m //.//");
+    system("git commit -m .");
     system("git push");
 
     printf("\n\n.............................\n");
