9eb659f80ee2f9452c2ef31a67cb7d10d53f6d55
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-03-02 14:48:28 +0000

    //.//

--- recursion_rev_sum.c
+++ recursion_rev_sum.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 int sum(int a)
 {
-    int i = 0;
+    int i = 1; //! this will cause problem cause for each function call i set to 1;
     if (i == a)
+    {
+        printf("\n%d", a);
         return a;
+    }
     else
+    {
+        printf("\n%d - sum(%d+1)", a, i);
         return a - sum(i + 1);
+    }
 }
 int main()
 {
