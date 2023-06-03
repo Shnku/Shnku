1c6ea3cac639aca84f26bd83586be94b5783bf47
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-06-03 19:18:52 +0530

    .

---
+++ starpattern.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\nen no of row...");
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n - 1; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        for (int k = 1; k <= 2 * n - 2 * i - 1; k++)
+        {
+            printf("-");
+        }
+        for (int l = 1; l <= i; l++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+// output..
+/*
+en no of row...5
+*-------*
+**-----**
+***---***
+****-****
+*/
