67d28aa9033645b0202cb08542ed8ede94507088
shanku <shankubag04@gmail.com>
2023-08-04 09:59:11 +0530

    .

--- common_prefix.c
+++ common_prefix.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 char *get_prefix(char **str, int len)
 {
+    if (len == 0)
+        return "";
+
     char *temp = NULL;
     int i;
     for (i = 0; str[0][i] != '\0'; i++)
@@ -10,10 +13,12 @@ char *get_prefix(char **str, int len)
         // make i+2 to fix that error...cause no space for null allocated
         temp[i] = str[0][i];
     }
-    temp[i] = '\0'; // i got an error in leetcode
+    temp = (char *)realloc(temp, (i + 1) * sizeof(char));
+    temp[i] = '\0';
     for (int i = 1; i < len; i++)
     {
-        for (int j = 0; str[i][j]; j++)
+        int j;
+        for (j = 0; str[i][j]; j++)
         {
             if (temp[j] != str[i][j])
             {
@@ -21,6 +26,7 @@ char *get_prefix(char **str, int len)
                 break;
             }
         }
+        temp[j] = '\0';
     }
     printf("%s", temp);
     // char *t = temp;
@@ -36,7 +42,7 @@ void test(char *str)
 
 int main()
 {
-    char *str[] = {"thisisdffds1", "thision", "thisition"};
+    char *str[] = {"this", "thi", "th"};
     // test(str[0]);
     char *a;
     a = get_prefix(str, 3);
