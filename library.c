fde04b2c4fb20d10e4b48a009e1236db5e1fc859
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-08-14 18:36:04 +0530

    .

--- library.c
+++ library.c
@@ -1,35 +1,4 @@
-/*
-Snow Howler is the librarian at the central library of the city of HuskyLand. He must handle requests which come in the following forms:
 
-1 x y : Insert a book with y pages at the end of the xth shelf.
-2 x y : Print the number of pages in the yth book on the xth shelf.
-3 x : Print the number of books on the xth shelf.
-
-Snow Howler has got an assistant, Oshie, provided by the Department of Education. Although inexperienced, Oshie can handle all of the queries of types 2 and 3.
-*/
-
-/*
-Sample Input 0.............
-5
-5
-1 0 50
-1 0 20
-1 2 78
-2 2 0
-3 0
-
-Sample Output 0.............
-78
-2
-
-Explanation 0.....................................
-There are 5 shelves and 5 requests,or queries.
-- 1 Place a 50 page book at the end of shelf 0.
-- 2 Place a 20 page book at the end of shelf 0.
-- 3 Place a 78 page book at the end of shelf 2.
-- 4 The number of pages in the 0th book on the shelf 2 is 78.
-- 5 The number of books on the 0th shelf is 2.
-*/
 
 #include <stdio.h>
 #include <stdlib.h>
