92eb88f1f3cd7f77ca41f50a70dd05929d64bdc0
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-08-29 08:28:52 +0530

    .

--- keyboard_common_matchs.c
+++ keyboard_common_matchs.c
@@ -12,19 +12,54 @@ the third row consists of the characters "zxcvbnm".
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-const char *keyboard[] = {"qwertyuiop", "asdfghjkl", "zxcvbnm", NULL};
-// NULL is seninel value..
+const char *keyboard[] = {"qwertyuiop", "asdfghjkl", "zxcvbnm", NULL}; // NULL is seninel value..
+void chek_memallocation(char *p);
 
 char *extract_word(char *s, int start, int end)
 {
-    char *p = calloc((end - start + 1), sizeof(char));
-    for (int i = start; i < end || *s == '\0'; i++)
+    char *p = (char *)calloc((end - start + 1), sizeof(char));
+    chek_memallocation(p);
+    int j = 0; // we should do this..
+    for (int i = start; i < end && s[i] != '\0'; i++)
     {
-        *p = s[i];
-        p++;
+        p[j++] = s[i]; //*p=s[i] //!wrong
     }
-    *p = '\0';
-    return p; // free it in main;
+    p[j] = '\0';
+    return p; // free it in main; //!p-end-start+1;
+}
+
+int main()
+{
+    char str[] = "this is a demo sentense";
+    int start = 0;
+    char *word = extract_word(str, 0, 5);
+    printf("\nwor %s", word);
+    free(word);
+    // for (int end = 1; str[end]; end++)
+    // {
+    //     char *a = NULL;
+    //     if (str[end] == ' ')
+    //     {
+    //         a = extract_word(str, start, end);
+    //         start = end;
+    //     }
+    //     for (int i = 0; keyboard[i] != NULL; i++)
+    //     {
+    //         int status = strcmp(keyboard[i], a);
+    //         printf("%d", status);
+    //     }
+    //     free(a);
+    // }
+    return 0;
+}
+
+void chek_memallocation(char *p)
+{
+    if (!p)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 int get_prefix(char *str, char *word)
@@ -38,14 +73,4 @@ int get_prefix(char *str, char *word)
         word++;
     }
     return point == strlen(word);
-}
-
-
-
-int main()
-{
-    char *a;
-    printf("\nthe output is:%s", a);
-    free(a);
-    return 0;
 }
