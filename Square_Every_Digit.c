113f5ae05b2cbc6c120681c0e4da53964b53beda
ShAnku <101804633+Shnku@users.noreply.github.com>
2023-09-09 18:01:12 +0530

    0 not coming error

--- Square_Every_Digit.c
+++ Square_Every_Digit.c
@@ -24,11 +24,11 @@ int sqr_each_digit(int no)
     while (no)
     {
         int rem = no % 10;
-        if (rem == 0)
-        {
-            rem = 1;
-            // not work ..
-        }
+        // if (rem == 0)
+        // {
+        //     rem = 1;
+        //     // not work ..
+        // }
         int p = (int)power(rem, 2);
         int n = no_of_digit(sum);
         sum = p * power(10, n) + sum;
@@ -37,12 +37,36 @@ int sqr_each_digit(int no)
     return sum;
 }
 
+int square_each_digit2(int no)
+{
+    int sum = 0;
+    while (no)
+    {
+        int digit_no = no_of_digit(no);
+        printf("\ndigit no = %d ", digit_no);
+        int tent = power(10, digit_no - 1);
+        printf("\ttens = %d ", tent);
+        int qus = no / tent;
+        printf("\nqus = %d ", qus);
+        int p = power(qus, 2);
+        printf("\tpower = %d ", p);
+        int dig_of_pow = no_of_digit(p);
+        printf("\tdig of power = %d ", dig_of_pow);
+        sum = sum * power(10, dig_of_pow) + p;
+        printf("\nsum=%d  no=%d\n", sum, no % tent);
+        no = no % tent;
+    }
+    return sum;
+}
+
 int main()
 {
-    printf("\n%d", sqr_each_digit(4351));
+    printf("\n%d", square_each_digit2(205309)); // 4 0 25 9 0 81
+    // printf("\n%d", sqr_each_digit(4351));
     // 16*10^4 + 9*10^3 + 25*10^1 +1
-    printf("\n%d", sqr_each_digit(1001));   // 1111
-    printf("\n%d", sqr_each_digit(500507)); // 25 1 1 25 1 49
+    // printf("\n%d", sqr_each_digit(1001));       // 1111
+    // printf("\n%d", sqr_each_digit(500507));     // 25 1 1 25 1 49
+    // printf("\n%d", square_each_digit2(500507)); // 25 1 1 25 1 49
 
     return 0;
 }
@@ -54,4 +78,24 @@ $./Square_Every_Digit
 
 169251
 11⏎ //?how to solve this..
+*/
+
+/*
+digit no = 6    tens = 100000
+qus = 2         power = 4       dig of power = 1
+sum=4  no=5309
+
+digit no = 4    tens = 1000
+qus = 5         power = 25      dig of power = 2
+sum=425  no=309
+
+digit no = 3    tens = 100
+qus = 3         power = 9       dig of power = 1
+sum=4259  no=9
+
+digit no = 1    tens = 1
+qus = 9         power = 81      dig of power = 2
+sum=425981  no=0
+
+425981 //! it should be 40259081.. not works..
 */
