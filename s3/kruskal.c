6a70cc3935f240b5eadb3cc2abcb8f935e4a002c
ShAnku <101804633+Shnku@users.noreply.github.com>
2024-01-07 16:46:04 +0530

    now kruskal works here but how??

--- s3/kruskal.c
+++ s3/kruskal.c
@@ -1,84 +1,88 @@
 #include <stdio.h>
-#define MAX 10
-void kruskal(int a, int arr[MAX][MAX]);
-int find(int, int *);
-int uni(int, int, int *);
+#include <stdlib.h>
 
-int main()
+int i, j, k, a, b, u, v, n, ne = 1;
+int min, mincost = 0, cost[9][9], parent[9];
+
+int find(int);
+int uni(int, int);
+
+void main()
 {
-    int mat[MAX][MAX], n;
-    printf("\nnp of row and col= ");
+    printf("Kruskal's algorithm in C\n");
+    printf("========================\n");
+
+    printf("Enter the no. of vertices:\n");
     scanf("%d", &n);
-    printf("\nFill the matrix...\n");
-    for (int i = 0; i < n; i++)
+
+    printf("\nEnter the cost adjacency matrix:\n");
+    for (i = 1; i <= n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (j = 1; j <= n; j++)
         {
-            scanf("%d", &mat[i][j]);
-            if (mat[i][j] == 0)
-                mat[i][j] = 999;
+            scanf("%d", &cost[i][j]);
+            if (cost[i][j] == 0)
+                cost[i][j] = 999;
         }
     }
-    kruskal(n, mat);
-    return 0;
-}
 
-void kruskal(int n, int arr[MAX][MAX])
-{
-    int ne = 0, min, parent[MAX], mincost = 0;
-    int a, u;
-    int b, v;
+    printf("The edges of Minimum Cost Spanning Tree are\n");
     while (ne < n)
     {
-        for (int i = 1, min = 999; i <= n; i++)
+        for (i = 1, min = 999; i <= n; i++)
         {
-            for (int j = 1; j <= n; j++)
+            for (j = 1; j <= n; j++)
             {
-                if (arr[i][j] < min)
+                if (cost[i][j] < min)
                 {
-                    min = arr[i][j];
+                    min = cost[i][j];
                     a = u = i;
                     b = v = j;
                 }
             }
         }
 
-        u = find(u, parent);
-        v = find(v, parent);
+        u = find(u);
+        v = find(v);
 
-        if (uni(u, v, parent))
+        if (uni(u, v))
         {
             printf("%d edge (%d,%d) =%d\n", ne++, a, b, min);
             mincost += min;
         }
 
-        arr[a][b] = arr[b][a] = 999;
+        cost[a][b] = cost[b][a] = 999;
     }
+
     printf("\nMinimum cost = %d\n", mincost);
 }
 
-int find(int i, int *parent)
+int find(int i)
 {
-    while (parent[i])     //error
-        i = parent[i]; // whart happns..
+    while (parent[i])
+        i = parent[i];
     return i;
 }
 
-int uni(int i, int j, int *parent)
+int uni(int i, int j)
 {
     if (i != j)
     {
-        parent[i] = j; // still seg fault if change i-j
+        parent[j] = i;
         return 1;
     }
+
     return 0;
 }
 
-/*
-some new issue
-np of row and col= 7
 
-Fill the matrix...
+/*
+Kruskal's algorithm in C
+========================
+Enter the no. of vertices:
+7
+
+Enter the cost adjacency matrix:
 0 2 0 7 3 0 0
 2 0 2 0 6 4 0
 0 2 0 5 4 2 1
@@ -86,25 +90,13 @@ Fill the matrix...
 3 6 4 1 0 0 0
 0 4 2 0 0 0 0
 0 0 1 6 0 0 0
-0 edge (7,2) =32767
-1 edge (2,6) =32767
-2 edge (3,4) =32767
+The edges of Minimum Cost Spanning Tree are
+1 edge (3,7) =1
+2 edge (4,5) =1
+3 edge (1,2) =2
+4 edge (2,3) =2
+5 edge (3,6) =2
+6 edge (1,5) =3
 
-
-*/
-
-/*
-output now
-np of row and col= 7
-
-Fill the matrix...
-0 2 0 7 3 0 0
-2 0 2 0 6 4 0
-0 2 0 5 4 2 1
-7 0 5 0 1 0 6
-3 6 4 1 0 0 0
-0 4 2 0 0 0 0
-0 0 1 6 0 0 0
-0 edge (4,7) =32767
-
+Minimum cost = 11
 */
